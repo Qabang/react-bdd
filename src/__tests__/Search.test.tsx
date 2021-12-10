996502c0f15a5d15fc0a404037a92440bb2ec071
Hampus A <hampzz99@gmail.com>
2021-12-10 10:24:40 +0100

    Test - Case Insensitive Search

--- src/__tests__/Search.test.tsx
+++ src/__tests__/Search.test.tsx
@@ -1,43 +1,54 @@
-
-import { mount } from 'enzyme';
-import StartView from '../components/Startview';
-import { BrowserRouter } from 'react-router-dom'
+import { mount } from "enzyme";
+import StartView from "../components/Startview";
+import { BrowserRouter } from "react-router-dom";
+import { wrap } from "module";
 
 const product = [
   {
     id: 1,
-    name: 'Bahama Mama Bronzer',
+    name: "Bahama Mama Bronzer",
     price: 149,
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, illo eum ducimus, sunt deserunt qui esse facilis dolore perferendis autem necessitatibus aspernatur cumque molestiae distinctio numquam quaerat, velit dolores corporis?',
-    image: '',
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, illo eum ducimus, sunt deserunt qui esse facilis dolore perferendis autem necessitatibus aspernatur cumque molestiae distinctio numquam quaerat, velit dolores corporis?",
+    image: "",
   },
-]
+];
 
-
-describe('Test for Serch on product', () => {
-
+describe("Test for Serch on product", () => {
   test('Search "Bahama", should render 1 product', () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <StartView products={product} />
+      </BrowserRouter>
+    );
+    const searchText = "Bahama";
+    const searchField = wrapper.find('[data-test="searchProduct"]');
 
-    const wrapper = mount(<BrowserRouter><StartView products={product} /></BrowserRouter>);
-    const searchText = 'Bahama'
-    const searchField = wrapper.find('[data-test="searchProduct"]')
-
-    searchField.simulate('change', { target: { value: searchText } })
-    expect(wrapper.find('[data-test="resultProduct"]').length).toBe(1)
-  })
+    searchField.simulate("change", { target: { value: searchText } });
+    expect(wrapper.find('[data-test="resultProduct"]').length).toBe(1);
+  });
   test('Search "Mascara", should render 0 products', () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <StartView products={product} />
+      </BrowserRouter>
+    );
+    const searchText = "Mascara";
+    const searchField = wrapper.find('[data-test="searchProduct"]');
 
-    const wrapper = mount(<BrowserRouter><StartView products={product} /></BrowserRouter>);
-    const searchText = 'Mascara'
-    const searchField = wrapper.find('[data-test="searchProduct"]')
+    searchField.simulate("change", { target: { value: searchText } });
+    expect(wrapper.find('[data-test="resultProduct"]').length).toBe(0);
+  });
+  test('Search "bAhAmA" should render 1 product. (Case Insensitive Search)', () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <StartView products={product} />
+      </BrowserRouter>
+    );
+    const searchText = "bAhAmA";
+    const searchField = wrapper.find('[data-test="searchProduct"]');
 
-    searchField.simulate('change', { target: { value: searchText } })
-    expect(wrapper.find('[data-test="resultProduct"]').length).toBe(0)
-  })
-})
-
-
-
-
-
+    searchField.simulate("change", { target: { value: searchText } });
+    expect(wrapper.find('[data-test="resultProduct"]').length).toBe(1);
+  });
+});
