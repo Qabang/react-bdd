b1af8804bd700babe274da28da7d457229247a98
Hampus A <hampzz99@gmail.com>
2021-12-10 10:02:12 +0100

    Case Insensitive Search

--- src/components/Startview.tsx
+++ src/components/Startview.tsx
@@ -1,21 +1,21 @@
-import ProductCard from './ProductCard'
-import { Product } from '../models/product'
-import { Link } from 'react-router-dom'
-import SearchBox from './search/SearchBox'
-import { useState } from 'react'
+import ProductCard from "./ProductCard";
+import { Product } from "../models/product";
+import { Link } from "react-router-dom";
+import SearchBox from "./search/SearchBox";
+import { useState } from "react";
 
 function Startview(props: { products: Array<Product> }) {
-  const products = props.products
-  const [searchText, setSearchText] = useState('')
+  const products = props.products;
+  const [searchText, setSearchText] = useState("");
   const filteredProducts = products.filter((product) =>
-    product.name.includes(searchText)
-  )
+    product.name.match(new RegExp(searchText, "i"))
+  );
   return (
     <>
       <h2>Våra produkter</h2>
       <SearchBox searchValue={searchText} setSearchValue={setSearchText} />
       {filteredProducts.map((product, id) => (
-        <Link key={product.id} to={`product/${product.id}`} >
+        <Link key={product.id} to={`product/${product.id}`}>
           <ProductCard
             id={product.id}
             name={product.name}
@@ -24,7 +24,7 @@ function Startview(props: { products: Array<Product> }) {
         </Link>
       ))}
     </>
-  )
+  );
 }
 
-export default Startview
+export default Startview;
